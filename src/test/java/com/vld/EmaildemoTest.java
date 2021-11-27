e28f797b64901578dee5c0dc85be8ba1ffa0ee95
vlada13 <vld.gta@gmail.com>
2021-11-27 13:12:34 +0000

    Feature: initial commit of micronaut app skeleton
    Added initial  skeleton of micronaut app for email sending

---
+++ src/test/java/com/vld/EmaildemoTest.java
@@ -0,0 +1,21 @@
+package com.vld;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class EmaildemoTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
